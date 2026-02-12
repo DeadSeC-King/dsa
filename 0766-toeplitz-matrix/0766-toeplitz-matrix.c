e08586ed059156a91a220c2c1f47f0a05108ee0f
Kushagra Ranjan <120331904+DeadSeC-King@users.noreply.github.com>
2026-02-12 10:15:24 +0530

    Time: 2 ms (4.38%), Space: 9.5 MB (36.5%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+
+bool isToeplitzMatrix(int** matrix, int matrixSize, int* matrixColSize) {
+    
+    int m = matrixSize;          // number of rows
+    int n = matrixColSize[0];    // number of columns
+    
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            
+            if (matrix[i][j] != matrix[i-1][j-1]) {
+                return false;
+            }
+        }
+    }
+    
+    return true;
+}
