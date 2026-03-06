7ef16026be200f6c452fba1d125cb6e8e762048a
Kushagra Ranjan <kushagraranjankr@gmail.com>
2026-03-06 20:54:49 +0530

    dd

--- source file(cw)/tr.c
+++ source file(cw)/tr.c
@@ -8,6 +8,30 @@ struct node *head=NULL;
 void insertbeg(int value){
     struct node *newnode;
     newnode=malloc(sizeof(struct node));
+    newnode->data=value;
+    newnode->next=head;
+    head=newnode;
+}
+void insertend(int value ){
+    struct node *temp;
+    struct node *newnode;
+    newnode=malloc(sizeof(struct node));
+    newnode->data=value;
+    if(head=NULL){
+        head=newnode;
+        return;
+    }
+    temp=head;
+    while (head!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=newnode;
+}
+void insertpos(int value , int pos ){
+    struct node *newnode;
+    struct node *temp;
+    int i;
+    newnode=malloc(sizeof(struct node));
     
 }
-
