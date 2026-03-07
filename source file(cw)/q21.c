64925e101317782e2f50727321b50d6954ab1608
Kushagra Ranjan <kushagraranjankr@gmail.com>
2026-03-07 10:43:33 +0530

    kk

---
+++ source file(cw)/q21.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+union uni
+{
+    int cgpa;
+    int marks;
+};
+
+struct adresss
+{
+    char city[30];
+    int pin;
+};
+
+struct student
+{
+    char name[50];
+    char address[100];
+    int id;
+    struct adresss ad;
+    union uni u;
+    int type;   // 1 = cgpa , 2 = marks
+    struct student *next;
+};
+
+struct student *head = NULL;
+
+void insert()
+{
+    struct student *newnode = (struct student*)malloc(sizeof(struct student));
+
+    printf("Enter name: ");
+    scanf("%s", newnode->name);
+
+    printf("Enter address: ");
+    scanf("%s", newnode->address);
+
+    printf("Enter id: ");
+    scanf("%d", &newnode->id);
+
+    printf("Enter city: ");
+    scanf("%s", newnode->ad.city);
+
+    printf("Enter pin: ");
+    scanf("%d", &newnode->ad.pin);
+
+    printf("Enter 1 for cgpa and 2 for marks: ");
+    scanf("%d", &newnode->type);
+
+    if(newnode->type == 1)
+    {
+        printf("Enter cgpa: ");
+        scanf("%d", &newnode->u.cgpa);
+    }
+    else
+    {
+        printf("Enter marks: ");
+        scanf("%d", &newnode->u.marks);
+    }
+
+    newnode->next = NULL;
+
+    if(head == NULL)
+    {
+        head = newnode;
+    }
+    else
+    {
+        struct student *temp = head;
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+    }
+}
+
+void deleteNode()
+{
+    if(head == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct student *temp = head;
+    head = head->next;
+    free(temp);
+}
+
+void display()
+{
+    if(head == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct student *temp = head;
+
+    while(temp != NULL)
+    {
+        printf("\nName: %s\n", temp->name);
+        printf("Address: %s\n", temp->address);
+        printf("ID: %d\n", temp->id);
+        printf("City: %s\n", temp->ad.city);
+        printf("Pin: %d\n", temp->ad.pin);
+
+        if(temp->type == 1)
+            printf("CGPA: %d\n", temp->u.cgpa);
+        else
+            printf("Marks: %d\n", temp->u.marks);
+
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    int choice;
+
+    while(1)
+    {
+        printf("\n1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+            case 1:
+                insert();
+                break;
+
+            case 2:
+                deleteNode();
+                break;
+
+            case 3:
+                display();
+                break;
+
+            case 4:
+                exit(0);
+
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+
+    return 0;
+}
