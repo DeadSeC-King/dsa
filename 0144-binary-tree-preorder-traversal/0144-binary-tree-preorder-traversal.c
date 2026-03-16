b7c2e51729d17ed51a5a063ed51fb27effc12166
Kushagra Ranjan <120331904+DeadSeC-King@users.noreply.github.com>
2026-03-16 20:04:27 +0530

    Time: 0 ms (100%), Space: 9.4 MB (16.24%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+void preorder(struct TreeNode* root, int* result, int* index){
+    if(root == NULL) return;
+
+    result[*index] = root->val;
+    (*index)++;
+
+    preorder(root->left, result, index);
+    preorder(root->right, result, index);
+}
+
+int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+
+    int* result = (int*)malloc(sizeof(int) * 100); // assume max nodes 100
+    *returnSize = 0;
+
+    preorder(root, result, returnSize);
+
+    return result;
+}
