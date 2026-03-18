b4a4336166a5bb1f1b54967e5624541da76b58ee
Kushagra Ranjan <kushagraranjankr@gmail.com>
2026-03-18 16:27:42 +0530

    dine

--- 100daysofdsa/dsa46.c
+++ 100daysofdsa/dsa46.c
@@ -1,4 +1,4 @@
-//Implement the solution for this problem.
+//Level Order Traversal
 #include <stdio.h>
 #include <stdlib.h>
 struct Node {
@@ -7,17 +7,29 @@ struct Node {
     struct Node* right;
 };
 struct Node* createNode(int data) {
-    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
-    node->data = data;
-    node->left = node->right = NULL;
-    return node;
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
 }
-int countNodes(struct Node* root) {
-    if (root == NULL)
-        return 0; // Return 0 for null nodes
-    int leftCount = countNodes(root->left);
-    int rightCount = countNodes(root->right);
-    return leftCount + rightCount + 1; // Count current node
+void levelOrderTraversal(struct Node* root) {
+    if (root == NULL) {
+        return;
+    }
+    struct Node* queue[100];
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+    while (front < rear) {
+        struct Node* currentNode = queue[front++];
+        printf("%d ", currentNode->data);
+        if (currentNode->left != NULL) {
+            queue[rear++] = currentNode->left;
+        }
+        if (currentNode->right != NULL) {
+            queue[rear++] = currentNode->right;
+        }
+    }
 }
 int main() {
     struct Node* root = createNode(1);
@@ -25,6 +37,7 @@ int main() {
     root->right = createNode(3);
     root->left->left = createNode(4);
     root->left->right = createNode(5);
-    printf("Number of nodes in the binary tree: %d\n", countNodes(root));
+    printf("Level Order Traversal: ");
+    levelOrderTraversal(root);
     return 0;
 }
