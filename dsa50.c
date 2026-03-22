929b8bde92c88f49333a7c14151882e3d3590546
Kushagra Ranjan <kushagraranjankr@gmail.com>
2026-03-22 22:27:11 +0530

    done

---
+++ dsa50.c
@@ -0,0 +1,64 @@
+// BST Search
+#include <stdio.h>
+#include <stdlib.h>
+
+// A binary tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Function to insert a new node into the BST
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+// Function to search for a value in the BST
+struct Node* search(struct Node* root, int key) {
+    if (root == NULL || root->data == key) {
+        return root;
+    }
+    if (key < root->data) {
+        return search(root->left, key);
+    }
+    return search(root->right, key);
+}
+
+int main() {
+    struct Node* root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    int key = 40;
+    struct Node* result = search(root, key);
+    if (result != NULL) {
+        printf("Element %d found in the BST.\n", key);
+    } else {
+        printf("Element %d not found in the BST.\n", key);
+    }
+
+    return 0;
+}
