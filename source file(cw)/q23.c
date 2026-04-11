7bfcc1f3d8116ec79d349544016347db6e55d9f2
Kushagra Ranjan <kushagraranjankr@gmail.com>
2026-04-11 11:01:34 +0530

    do

---
+++ source file(cw)/q23.c
@@ -0,0 +1,54 @@
+//wap to create bst where random numbers are inserted and then print the elements in sorted order
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+
+    inorder(root->left);        
+    printf("%d ", root->data);  
+    inorder(root->right);       
+}
+int main() {
+    struct Node* root = NULL;
+    int n, data;
+
+    printf("Enter the number of elements to insert: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &data);
+        root = insert(root, data);
+    }
+
+    printf("Elements in sorted order: ");
+    inorder(root);
+    printf("\n");
+
+    return 0;
+}
